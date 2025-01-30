b4dfe0e9f67d8a3ca42bb5cdbe6cde74d2b1ea1c
KelseyDang2002 <kelseydang813@gmail.com>
2025-01-29 16:39:52 -0800

    redo entire code

--- main.py
+++ main.py
@@ -7,84 +7,118 @@ BAUD = 115200
 # cycling paramters by default
 ON_TIME = 800
 OFF_TIME = 200
+NUM_CYCLES = 100
 
 # xiao = serial.Serial(port='/dev/ttyACM0', baudrate=BAUD, timeout=1) # linux
 xiao = serial.Serial(port='COM3', baudrate=BAUD, timeout=1) # windows
-time.sleep(2)
+time.sleep(1)
 
-def control_state(state):
-    if state == "on":
-        xiao.write(b'1') # tell RP2040 to turn on MOSFET
-    
-    elif state == "off":
-        xiao.write(b'0') # tell RP2040 to turn off MOSFET
+# prompt user for parameters
+def promptParameters(on_time, off_time, num_cycles):
+    while True:
+        try:
+            print("Enter -1 to use default parameters.")
 
-    else:
-        print("Invalid state.")
+            on_delay = int(input("On delay (ms): "))
+            if on_delay > 0:
+                on_time = on_delay
+            elif on_delay == -1:
+                print("Use default on delay")
+            else:
+                return
+            
+            off_delay = int(input("Off delay (ms): "))
+            if off_delay > 0:
+                off_time = off_delay
+            elif off_delay == -1:
+                print("Use default off delay")
+            else:
+                return
+            
+            cycles = int(input("Number of cycles: "))
+            if cycles > 0:
+                num_cycles = cycles
+            elif cycles == -1:
+                print("Use default number of cycles")
+            else:
+                return
+            
+            print(f"Selected:\n\tOn delay: {on_time}ms\n\tOff_delay: {off_time}ms\n\tCycles: {num_cycles}\n")
+            return on_time, off_time, num_cycles
+        
+        except TypeError:
+            print("Invalid input. Only integer types allowed.")
 
-def parameters(on_time, off_time):
-    on_delay = int(input("On delay (ms) (enter -1 to use default): "))
-    off_delay = int(input("Off delay (ms) (enter -1 to use default): "))
-    num_cycles = int(input("Number of cycles: "))
+        except ValueError:
+            print("Invalid input. Only numbers allowed.")
 
-    if on_delay > 0:
-        on_time = on_delay
-    elif on_delay == -1:
-        print(f"Default: {on_time} ms")
-    else:
-        return False
-    
-    if off_delay > 0:
-        off_time = off_delay
-    elif off_delay == -1:
-        print(f"Default: {off_time} ms")
-    else:
-        return False
-    
-    if num_cycles < 0:
-        return False
-
-    return on_delay, off_delay, num_cycles
-
-def flash(on_time, off_time, num_cycles):
+# function for flashing LED
+def flash(on_delay, off_delay, num_cycles):
     print("Calling flash function...\n")
-
-    # convert params to string before sending to RP2040
+    
     for i in range(num_cycles):
-        xiao.write(b'1')
-        print("Light On")
-        xiao.write(str(on_time).encode()) # send on delay to RP2040
-
         xiao.write(b'0')
-        print("Light Off")
-        xiao.write(str(off_time).encode()) # send off delay to RP2040
+        time.sleep(off_delay / 1000)
+        # TODO: send off delay to RP2040
+        
+        xiao.write(b'1')
+        time.sleep(on_delay / 1000)
+        # TODO: send on delay to RP2040
+        
+        print(f"{i + 1} cycle(s) completed")
+    return
+    
+# turned on
+def onState():
+    xiao.write(b'1') # tell RP2040 to turn on MOSFET
 
+    response = xiao.readline().decode('utf-8').strip()
+    if response:
+        print(f"XIAO RP2040 says: {response}\n")
+
+    on_delay, off_delay, num_cycles = promptParameters(ON_TIME, OFF_TIME, NUM_CYCLES)
+    flash(on_delay, off_delay, num_cycles)
+    return
+        
+# turned off
+def offState():
+    xiao.write(b'0') # tell RP2040 to turn off MOSFET
+
+    response = xiao.readline().decode('utf-8').strip()
+    if response:
+        print(f"XIAO RP2040 says: {response}\n")
+    return
 
 while True:
     try:
-        command = input("\nEnter 'on' or 'off' to control sign: ").strip()
-        control_state(command)
+        command = input("Enter 'on' or 'off' to control sign or 'q' to quit program: ").strip().lower()
 
-        response = xiao.readline().decode('utf-8').strip()
-        if response:
-            print(f"XIAO RP2040 says: {response}")
+        if command == "on":
+            onState()
+        elif command == "off":
+            offState()
+        elif command == "q":
+            print("\nExiting program...")
+            sys.exit(0)
+        else:
+            print("Invalid command. Try again.")
 
         # XIAO sends byte back to RPi
-        incomingChar = xiao.read().decode('utf-8')
-        if incomingChar == '0':
-            print(f"XIAO write: {incomingChar}")
+        # incomingChar = xiao.read().decode('utf-8')
+        # if incomingChar == '0':
+        #     print(f"XIAO write: {incomingChar}")
 
-        elif incomingChar == '1':
-            print(f"XIAO write: {incomingChar}")
+        # elif incomingChar == '1':
+        #     print(f"XIAO write: {incomingChar}")
             
-            on_time, off_time, num_cycles = parameters(ON_TIME, OFF_TIME)
+        #     on_time, off_time, num_cycles = parameters(ON_TIME, OFF_TIME)
 
-            flash(on_time, off_time, num_cycles)
+        #     flash(on_time, off_time, num_cycles)
 
-        else:
-            print("No incoming char.")
-            break
+        # else:
+        #     print("No incoming char.")
+        #     break
         
     except KeyboardInterrupt:
         print("\n\nProgram interrupted by user.")
-        sys.exit()
+        # sys.exit()
