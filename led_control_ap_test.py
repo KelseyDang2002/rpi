6ce5c08f4dbb05e4eae7e7cf54036e0f427ace96
KelseyDang2002 <kelseydang813@gmail.com>
2026-01-08 12:39:31 -0800

    update

--- led_control_ap_test.py
+++ led_control_ap_test.py
@@ -1,25 +1,24 @@
+import socket
 import requests
 import time
 import sys
 
-ESP32_IP = "192.168.1.74"
+ESP32_IP = "10.42.0.100"
+ESP32_PORT = 4210
 
-BAUD = 115200
-
-# paramters by default
-TIMEOUT = 60000 # 1 minute
+# parameters by default
+TIMEOUT = 10000
 ON_TIME = 1000
 OFF_TIME = 1000
 
-# prompt user for parameters
 def parameters(timeout, on_time, off_time):
     try:
         if len(sys.argv) > 5:
-            print(f"Input Error: '{sys.argv[0]}' has a maximum of 5 arguments. {len(sys.argv)} were given.")
+            print(f"Input Error: '{sys.argv[0]}' has a maximum of 5 arguments. {                                                                                                             len(sys.argv)} were given.")
             print("\nExiting program...")
             sys.exit(1)
 
-        # specify time_out, on_delay and off_delay
+        # specify time_out, on_delay, and off_delay
         if len(sys.argv) == 5:
             time_out = int(sys.argv[2]) # convert time_out argument to int
             if time_out > 0:
@@ -49,7 +48,7 @@ def parameters(timeout, on_time, off_time):
         if len(sys.argv) == 4:
             time_out = int(sys.argv[2]) # convert on_delay argument to int
             if time_out > 0:
-                timeout = time_out
+                    timeout = time_out
             else:
                 print("Input Error: Only integers larger than 0 are allowed.")
                 print("\nExiting program...")
@@ -87,39 +86,43 @@ def parameters(timeout, on_time, off_time):
         print("\nExiting program...")
         sys.exit(1)
 
-# turned on
-def onState():
+# send commands via UDP
+def send_udp(message: str):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(5.0)
     try:
-        timeout, on_delay, off_delay = parameters(TIMEOUT, ON_TIME, OFF_TIME)
-        # message = f"1,{timeout}, {on_delay},{off_delay}\n"
-        # xiao.write(message.encode()) # tell RP2040 to turn on MOSFET
-        url = f"http://{ESP32_IP}/ledon?timeout={timeout}&on={on_delay}&off{off_delay}"
-        response = requests.get(url).text
-        # time.sleep(1)
+        sock.sendto(message.encode(), (ESP32_IP, ESP32_PORT))
+        try:
+            data, _ = sock.recvfrom(1024)
+            print(f"XIAO says: ", data.decode())
+        except socket.timeout:
+            print("No response (timeout)")
+    finally:
+        sock.close()
 
-        # response = xiao.readline().decode('utf-8').strip()
-        if response:
-            print(f"XIAO RP2040 says: {response}\n")
-        return
+def onState():
+    timeout, on_delay, off_delay = parameters(TIMEOUT, ON_TIME, OFF_TIME)
+    message = f"1,{timeout},{on_delay},{off_delay}"
+    send_udp(message)
+    # session = requests.Session()
+    # response = session.get(f"http://{ESP32_IP}/ledon?timeout={timeout}&high={o                                                                                                             n_delay}&low={off_delay}")
+    # if response:
+    #     print(f"XIAO says: {response.text}")
+    # else:
+    #     print("No response.")
+    # return
 
-    except TypeError:
-        print("TypeError: cannot unpack non-iterable NoneType object")
-        print("\nExiting program...")
-        sys.exit(1)
-
-# turned off
 def offState():
-    # message = f"0\n"
-    # xiao.write(message.encode()) # tell RP2040 to turn off MOSFET
-    url = f"http://{ESP32_IP}/ledoff"
-    response = requests.get(url).text
+    message = "0"
+    send_udp(message)
+    # session = requests.Session()
+    # response = session.get(f"http://{ESP32_IP}/ledoff")
+    # if response:
+    #     print(f"XIAO says: {response.text}")
+    # else:
+    #     print(f"No response.")
+    # return
 
-    # response = xiao.readline().decode('utf-8').strip()
-    if response:
-        print(f"XIAO RP2040 says: {response}")
-    return
-
-# function to display help menu
 def helpMenu():
     print("********** HELP MENU **********")
     print("\nFormat for running command via CLI:")
@@ -127,11 +130,11 @@ def helpMenu():
     print("\n\t1 [file.py] (required)")
     print("\t\t- name of the file/program")
     print("\n\t2 [action] (required)")
-    print("\t\ton - tell RP2040 microcontroller to turn on")
-    print("\t\toff - tell RP2040 microcontroller to turn off")
+    print("\t\ton - tell XIAO microcontroller to turn on")
+    print("\t\toff - tell XIAO microcontroller to turn off")
     print("\t\thelp/h - help menu/how to run script")
     print("\n\t3 [timeout]")
-    print("\t\t- the time it takes for flashing to turn off after the on command")
+    print("\t\t- the time it takes for flashing to turn off after the on command                                                                                                             ")
     print("\t\t- omit parameter to use default timeout")
     print(f"\t\t- default is {TIMEOUT}ms unless specified")
     print("\n\t4 [on_delay]")
@@ -143,33 +146,27 @@ def helpMenu():
     print("\t\t- omit parameter to use default off delay")
     print(f"\t\t- default is {OFF_TIME}ms unless specified")
 
-# function to handle command line arguments
 def handleCommands():
     print(f"\nEntered: {str(sys.argv)}\n")
 
     if len(sys.argv) < 2:
-        print(f"Run Error: '{sys.argv[0]}' requires at least {2 - len(sys.argv)} more command line argument.")
+        print(f"Run Error: '{sys.argv[0]}' requires at least {2 - len(sys.argv)}                                                                                                              more command line argument.")
         print(f"Run command 'python {sys.argv[0]} help' to bring up help menu.")
         sys.exit(1)
 
     action = sys.argv[1].lower()
     if action == 'on':
         onState()
-
     elif action == 'off':
         offState()
-
     elif action == 'help' or action == 'h':
         helpMenu()
-
     else:
         print(f"Action Error: '{action}' is an invalid argument.")
         print(f"Run command 'python {sys.argv[0]} help' to bring up help menu.")
-        sys.exit(1)
 
-# main
 if __name__ == "__main__":
     print(f"\nExecuting program {sys.argv[0]}...")
     handleCommands()
-    print("\nExiting program...")
+    print(f"\nExiting program...")
     sys.exit(0)
